732fa495e7cb7f6639c7f5b6b160c21ffab025b5
Olachi Efika <olachi_efika@yahoo.com>
2022-11-22 19:35:57 -0500

    Working on new lesson

---
+++ ex_13_03.py
@@ -0,0 +1,25 @@
+import urllib.request, urllib.parse, urllib.error
+import json
+
+api_key = False
+
+if api_key is False:
+    api_key = 42
+    serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+
+address = input('Enter location: ')
+
+parms = dict()
+parms['address'] = address
+if api_key is not False: parms['key'] = api_key
+url = serviceurl + urllib.parse.urlencode(parms)
+
+print('Retrieving', url)
+uh = urllib.request.urlopen(url)
+data = uh.read().decode()
+print('Retrieved', len(data), 'characters')
+
+js = json.loads(data)
+
+for place in js['results']:
+print("Place id ", place['place_id']
